09fb7aa3e2c1d2c9285609f0917c13738d524f16
bultet <jean-philippe.bultet@wanadoo.fr>
2021-12-19 21:28:07 +0100

    Base

--- AOC_2021_01.dpr
+++ AOC_2021_01.dpr
@@ -10,6 +10,10 @@ uses
 begin
   try
     // Day 1 : sonar sweep
+    // Github configured
+
+    writeln('Le programme fonctionne');
+    readln;
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
