e9e81d43b13af8fb500e45f74d3924f364fb8066
jean-philippe.bultet@wanadoo.fr <bultet>
2021-12-20 23:17:15 +0100

    Day_1 : proposition

--- AOC_2021_01.dpr
+++ AOC_2021_01.dpr
@@ -7,12 +7,41 @@ program AOC_2021_01;
 uses
   System.SysUtils;
 
+var
+   F : text;
+   increment, i, val_prec : integer;
+   val : string;
+
 begin
   try
     // Day 1 : sonar sweep
     // Github configured
-
     writeln('Day 1 : sonar sweep');
+
+    // Ouvre le fichier input
+    AssignFile(F,'..\..\01\input.txt');
+    Reset(F);
+
+    // Initialise les variables
+    increment := 0;
+    i := 0;
+    val_prec := 0;
+
+
+    // Boucle sur toutes les lignes du fichier
+    Readln(F, val);
+    val_prec := val.ToInteger;
+
+    repeat
+      Readln(F, val);
+      i := val.ToInteger;
+      if i>val_prec then inc(increment);
+
+    until eof(F);
+
+    writeln('Nb increment : ', increment);
+
+
     write('Pressez ENTRER');
     readln;
   except
